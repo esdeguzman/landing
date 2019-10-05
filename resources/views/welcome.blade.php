a0f093405e43f2ab07012ea39fc6605b538aef88
Esmeraldo <deguzman.esmeraldo@gmail.com>
2019-10-05 20:24:42 +0800

    =Removal of Google AdSense script

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,13 +46,6 @@
         }
       }
     </style>
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-         (adsbygoogle = window.adsbygoogle || []).push({
-              google_ad_client: "ca-pub-6429484135715885",
-              enable_page_level_ads: true
-         });
-    </script>
   </head>
 
   <body>
